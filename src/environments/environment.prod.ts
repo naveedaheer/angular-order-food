fe645eb76ceb0cc96cbba8fa6dc47a843b90ed03
Naveed Aheer <naveedaheerpc@gmai.com>
2022-12-11 04:05:15 +0500

    Added API base URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiURL: ''
+  apiURL: 'https://food-ordering.herokuapp.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiURL: ''
+  apiURL: 'https://food-ordering.herokuapp.com'
 };
 
 /*
