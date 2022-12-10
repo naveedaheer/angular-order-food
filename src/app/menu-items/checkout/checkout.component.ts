99b3fcbbc62d02765a14f6dc41b5e8e8381e076b
Naveed Aheer <naveedaheerpc@gmai.com>
2022-12-11 04:15:26 +0500

    Added thank you message

--- src/app/menu-items/checkout/checkout.component.ts
+++ src/app/menu-items/checkout/checkout.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./checkout.component.scss']
 })
 export class CheckoutComponent implements OnInit {
-
+  submitted = false
   constructor() { }
 
   ngOnInit(): void {
