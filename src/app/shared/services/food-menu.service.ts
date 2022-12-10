8c8d7cd40cdb71ba8c978e8c561c023a5e1f1ad8
Naveed Aheer <naveedaheerpc@gmai.com>
2022-12-11 04:04:55 +0500

    Added API base URL

--- src/app/shared/services/food-menu.service.ts
+++ src/app/shared/services/food-menu.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { WebApiService } from './web-api.service';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class FoodMenuService {
 
-  constructor() { }
+  constructor(private webApi: WebApiService) { }
+
+  getMenuItems(): Observable<any> {
+    return this.webApi.get<any>("/order-book/menu-items");
+  }
+
 }
