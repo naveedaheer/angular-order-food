f3afa1a38f50a3f116ff801b48c196ce298930c6
Naveed Aheer <naveedaheerpc@gmai.com>
2022-12-11 04:05:34 +0500

    Integrated API to display menu items

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { FoodMenuService } from 'src/app/shared/services/food-menu.service';
+
+interface Item {
+  availableQty: number;
+  description: string;
+  id: number;
+  imageUrl: string;
+  name: string;
+  price: number;
+}
 
 @Component({
   selector: 'app-home',
@@ -6,10 +16,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  items: Item[] = []
 
-  constructor() { }
+  constructor(private menuService: FoodMenuService) { }
 
   ngOnInit() {
+    this.menuService.getMenuItems().subscribe(res => {
+      this.items = res
+    })
   }
 
 }
